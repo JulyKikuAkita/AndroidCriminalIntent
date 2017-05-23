5864713b7200e746e879d30efdf2e3e32870e6c8
IFang Lee <ilee@ifwe.co>
2017-05-22 17:05:10 -0700

    Adding implicit intent for contacts
    Add implicit intent to call contacts

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,12 +1,21 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -22,6 +31,8 @@ import android.widget.Toast;
 import java.util.Date;
 import java.util.UUID;
 
+import static android.text.TextUtils.join;
+
 public class CrimeFragment extends Fragment {
     private String TAG = getClass().getCanonicalName();
     private static final String ARG_CRIME_ID = "crime_id";
@@ -29,6 +40,9 @@ public class CrimeFragment extends Fragment {
     private static final String DIALOG_TIME = "DialogTIME";
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_TIME = 1;
+    private static final int REQUEST_CONTACT = 11;
+    private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 100;
+    private static final int CALL_CONTACTS_PERMISSIONS_REQUEST = 101;
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -37,6 +51,8 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
     private Button mDeleteCrimeButton;
     private Button mCreateCrimeButton;
+    private Button mSuspectButton;
+    private Button mCalltButton;
     private Button mReportButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
@@ -158,16 +174,56 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //use share compat to create intent
+                Intent i = ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/plain")
+                        .setText(getCrimeReport())
+                        .setSubject(getString(R.string.crime_report_subject))
+                        .getIntent();
+            /*
+                //use self-created intent
                 Intent i = new Intent(Intent.ACTION_SEND);
                 i.setType("text/plain");
                 i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
                 i.putExtra(Intent.EXTRA_SUBJECT,
                         getString(R.string.crime_report_subject));
+            */
                 i = Intent.createChooser(i, getString(R.string.send_report));
                 startActivity(i);
             }
         });
 
+        final Intent pickContact = new Intent(Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI);
+        //pickContact.addCategory(Intent.CATEGORY_HOME); test package manager for contact intent
+        mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
+        mSuspectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(pickContact, REQUEST_CONTACT);
+            }
+        });
+
+        if (mCrime.getSuspect() != null) {
+            mSuspectButton.setText(mCrime.getSuspect());
+        }
+        PackageManager packageManager = getActivity().getPackageManager();
+        if (packageManager.resolveActivity(pickContact,
+                packageManager.MATCH_DEFAULT_ONLY) == null) {
+            mSuspectButton.setEnabled(false);
+        }
+
+        mCalltButton = (Button) v.findViewById(R.id.call_suspect);
+        mCalltButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = queryContactPhone();
+                Uri call = Uri.parse("tel:" + phoneNumber);
+                Intent callContact = new Intent(Intent.ACTION_DIAL);
+                callContact.setData(call);
+                startActivity(callContact);
+            }
+        });
         return v;
     }
 
@@ -187,6 +243,25 @@ public class CrimeFragment extends Fragment {
                 mCrime.setDate(time);
                 updateTime();
                 break;
+            case REQUEST_CONTACT:
+                if (data != null) {
+                    Uri contactUri = data.getData();
+                    String[] queryFields = new String[]{
+                            ContactsContract.Contacts.DISPLAY_NAME
+                    };
+
+                    Cursor c = getActivity().getContentResolver()
+                            .query(contactUri, queryFields, null, null, null);
+                    try{
+                        c.moveToFirst();
+                        String suspect = c.getString(0);
+                        mCrime.setSuspect(suspect);
+                        mSuspectButton.setText(suspect);
+                    }
+                    finally {
+                        c.close();
+                    }
+                }
         }
     }
 
@@ -220,5 +295,120 @@ public class CrimeFragment extends Fragment {
                 solvedString,
                 suspect);
         return report;
+    }
+
+    public String queryContactsId() {
+        String[] queryFields = new String[]{
+                ContactsContract.Contacts.DISPLAY_NAME, "=", "'" + mCrime.getSuspect() + "'"
+        };
+        getPermissionToReadContacts();
+        Cursor c = getActivity().getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                null, join(" ", queryFields), null, null);
+        try{
+            if(c.getCount() > 0) {
+                c.moveToFirst();
+                String str = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                return str;
+            }
+        } finally {
+            c.close();
+        }
+        throw new IllegalArgumentException("Cannot get contact ID: " + mCrime.getSuspect());
+    }
+
+    public String queryContactPhone() {
+        String contactId = queryContactsId();
+        String[] queryFields = new String[] {
+                CommonDataKinds.Phone.CONTACT_ID, "=", "'" + contactId + "'"
+        };
+        getPermissionToCallContacts();
+        Cursor phones = getActivity().getContentResolver().query(
+                CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                join(" ", queryFields),
+                null,
+                null
+        );
+        try{
+            if(phones.getCount() > 0) {
+                phones.moveToFirst();
+                return phones.getString(phones.getColumnIndex(CommonDataKinds.Phone.NUMBER));
+            }
+        }catch(Exception e) {
+            return "fake nmber 123456789";
+        }finally{
+            phones.close();
+        }
+        throw new IllegalArgumentException("Cannot get phone number for contact " + mCrime.getSuspect());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode
+            , @NonNull String[] permissions
+            , @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case READ_CONTACTS_PERMISSIONS_REQUEST:
+            case CALL_CONTACTS_PERMISSIONS_REQUEST:
+                if (grantResults.length == 1 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), R.string.permission_granted, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), R.string.permission_denied, Toast.LENGTH_SHORT).show();
+
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void getPermissionToReadContacts(){
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    public void getPermissionToCallContacts(){
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.CALL_PHONE)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE},
+                    CALL_CONTACTS_PERMISSIONS_REQUEST);
+        }
     }
 }
